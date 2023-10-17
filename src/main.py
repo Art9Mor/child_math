be52afe2f5985808782566e3e42b4e79473884e1
artem <deusestmaleficus@gmail.com>
2023-10-17 15:54:07 +0700

    adding of information

--- src/main.py
+++ src/main.py
@@ -3,14 +3,24 @@ from time import time
 
 if __name__ == '__main__':
 
+    # Спрашивает имя пользователя
     user_name = input('Enter your name\n').title()
-    print(f'Hello {user_name}!\n Here is your examples with division\n You have 5 minutes to decide.\n')
 
+    # Приветствие
+    print(f'Hello {user_name}!\n Here is your examples with division\n You have 5 minutes to decide them.\n')
+
+    # список для примеров
     examples = []
+    # писок с ответами
     answers = []
 
 
     def expresisons() -> dict:
+        """
+        Функция создаёт примеры из рандомно взятых чисел из определённого диапазона, отфильтровывает
+        и заносит их в список и отдельно ответы в другой список, после чего склеивает их в словарь.
+        :return:
+        """
         count = 0
         while count < 30:
             x = randint(1, 1000)
@@ -26,19 +36,25 @@ if __name__ == '__main__':
 
 
     ex_dict = expresisons()
-    print(ex_dict)
+    # print(ex_dict)
+
+    # счётчик ответов
     score = 0
+
+    # счётчик вопросов
     answer_count = 0
 
-    counter = 1
-
+    # фиксатор времени
     start = time()
 
     for key, value in ex_dict.items():
         user_answer = input(f'{key} ')
+
+        # остановка программы
         if user_answer in ('stop', 'стоп'):
             print("You stop the game. Let's see your results...\n")
             break
+        # проверка ответов
         elif user_answer == value:
             score += 1
             answer_count += 1
@@ -48,9 +64,11 @@ if __name__ == '__main__':
             print('Wrong answer!')
             continue
 
+    # подсчёт вреемни на решение
     ending = time()
     time_counter = str((ending - start) / 60)
 
+    # результаты
     print(f'{user_name}, your time is {time_counter[:4]} min.\nCorrect answers are {score} from {answer_count}.')
 
     if score == answer_count and score >= 10:
